592e29d6f0402694975ad3c54d8c4dd01a1fce16
ray2480 <ray2480@keio.jp>
2017-12-11 18:08:49 +0900

    def quantizeOnsetFramesPer16thNote

--- code/functions.py
+++ code/functions.py
@@ -1,29 +1,36 @@
-#clickで書き出すと16分音符毎にクリック音が鳴らせるようにビートトラッキングする
-#16分音符毎にインデックスが割り当てられている
-#beat_times_per_16thの値は秒数、サンプル、フレームのどれかで選べる
-def beatTrackPer16th(x, sr, bpm, units='frames', offset_16th_notes=0):
+hop_length = 256
+
+
+onset_envelope = librosa.onset.onset_strength(background_percussive, sr=sr, hop_length=hop_length)
+onset_frames = librosa.util.peak_pick(onset_envelope, 7, 7, 7, 7, 0.8, 5)
+
+def beatTrackPer16th(x, bpm, sr=22050, hop_length=512, units='frames', offset_16th_notes=0):
+  """
+  clickで書き出すと16分音符毎にクリック音が鳴らせるようにビートトラッキングする
+  16分音符毎にインデックスが割り当てられている
+  beat_frames_per_16thの値は秒数、サンプル、フレームのどれかで選べる
+  offset_16th_notes（最初の16分音符の数）でアウフタクトの除去が可能
+  """
   if units == 'time':
-    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=bpm, units='time')
+    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, hop_length=hop_length, start_bpm=bpm, units='time')
   if units == 'samples':
-    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=bpm, units='samples')
+    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, hop_length=hop_length, start_bpm=bpm, units='samples')
   else:
-    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=bpm, units='frames')
-  beat_times_per_16th = []
+    tempo, beat_times = librosa.beat.beat_track(x, sr=sr, hop_length=hop_length, start_bpm=bpm, units='frames')
+  beat_frames_per_16th = []
   for i in range(len(beat_times) - 1):
     interval_per_16th_units = np.linspace(beat_times[i], beat_times[i+1], 5)
-    beat_times_per_16th = np.hstack((beat_times_per_16th, interval_per_16th_units))
+    beat_frames_per_16th = np.hstack((beat_frames_per_16th, interval_per_16th_units))
   if offset_16th_notes > 0:
-    #アウフタクトの除去
-    beat_times_per_16th = beat_times_per_16th[offset_16th_notes:]
-  return beat_times_per_16th
+    beat_frames_per_16th = beat_frames_per_16th[offset_16th_notes:]
+  return beat_frames_per_16th
 
-def quantizeOnsetFramesBeatsToIntempo16Beats(onset_frames, beat_times_per_16th, sr, hop_length)
-  beat_times_omitted_quantized = []
-  sixteenths_index = []
-  for onset in librosa.frames_to_time(onset_frames, sr=sr, hop_length=hop_length):
-    index = np.argmin(np.absolute(beat_times_sixteenths - onset))
-    sixteenths_index.append(index)
-    beat_times_omitted_quantized.append(beat_times_sixteenths[index])
-  sixteenths_index = np.array(sixteenths_index)
-  
-  return intempo_16beats_quantized_beats, sixteenths_index
+def quantizeOnsetFramesPer16thNote(onset_frames, beat_frames_per_16th):
+  quantized_onset_frames_per_16th_note = []
+  index_of_16th_notes = []
+  for onset in onset_frames:
+    index = np.argmin(np.absolute(beat_frames_per_16th - onset))
+    index_of_16th_notes.append(index)
+    quantized_onset_frames_per_16th_note.append(beat_frames_per_16th[index])
+  index_of_16th_notes = np.array(index_of_16th_notes)
+  return quantized_onset_frames_per_16th_note, index_of_16th_notes
