91fd257fe4577d993c3afec8944bb7e3778b66eb
ray2480 <ray2480@keio.jp>
2018-01-08 17:21:39 +0900

    Update forExperiments.py

--- code/forExperiments.py
+++ code/forExperiments.py
@@ -101,6 +101,34 @@ for i in range(len(input_audio_filename_array)):
   midi_filename = input_audio_filename + '.mid'
   createMidiRhythmScore(midi_filename, onset_frames_index_of_16th_notes, strong_onset_frames_index_of_16th_notes, weak_onset_frames_index_of_16th_notes, bpm, auftakt_16th_notes_number)
 
+##クリック音を原曲に入れる
+i = 0
+for i in range(len(input_audio_filename_array)):
+  input_audio_filename = input_audio_filename_array[i]
+  x, sr = librosa.load(input_audio_filename)
+  audio_filename_background_percussive = input_audio_filename + '_background_percussive.wav'
+  back, sr = librosa.load(audio_filename_background_percussive)
+  bpm = input_bpm_array[i]
+  auftakt_16th_notes_number = auftakt_16th_notes_number_array[i]
+  
+  #ビート処理
+  onset_envelope = librosa.onset.onset_strength(back, sr=sr, hop_length=hop_length)
+  #onset_frames = librosa.util.peak_pick(onset_envelope, 7, 7, 7, 7, 0.8, 5)
+  onset_frames = librosa.util.peak_pick(onset_envelope, 7, 7, 7, 7, 2.0, 5)
+  beat_frames_per_16th_note = trackBeatsPer16thNote(back, bpm, sr=sr, hop_length=hop_length, offset_16th_notes=0)
+  quantized_onset_frames_per_16th_note, onset_frames_index_of_16th_notes = quantizeOnsetFramesPer16thNote(onset_frames, beat_frames_per_16th_note)
+  strong_onset_frames, strong_onset_frames_index_of_16th_notes = getStrongOnsetFrames(onset_envelope, beat_frames_per_16th_note, onset_frames_index_of_16th_notes)
+  weak_onset_frames, weak_onset_frames_index_of_16th_notes = getWeakOnsetFrames(strong_onset_frames_index_of_16th_notes, onset_frames_index_of_16th_notes, beat_frames_per_16th_note)
+
+  #クリック音入力
+  N = len(x)
+  T = N/float(sr)
+  clicks_strong = librosa.clicks(frames=strong_onset_frames, sr=sr, hop_length=hop_length, length=N)
+  clicks_weak = librosa.clicks(frames=weak_onset_frames, sr=sr, hop_length=hop_length, click_freq=1000.0, click_duration=0.01, length=N)
+  
+  #audio_filename_clicks = input_audio_filename + '_clicks.wav'
+  audio_filename_clicks = input_audio_filename + '_clicks2.0.wav'
+  librosa.output.write_wav(audio_filename_clicks, x+clicks_strong+clicks_weak, sr)
 
 def separateMusicIntoForegroundAndBackground(x, sr):
   # compute the spectrogram magnitude and phase
